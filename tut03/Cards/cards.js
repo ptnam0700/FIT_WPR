0c4d06487f791817c873d82354506580731b0ded
Nam Pham <ptnam0700@gmail.com>
2021-11-06 00:25:20 +0700

    Click function

--- tut03/Cards/cards.js
+++ tut03/Cards/cards.js
@@ -1,18 +1,27 @@
 const card_container = document.querySelector('#cards-container');
 
-for(let i=0; i< CARDS.length; i++ ){
-    const cardSrc = CARDS[i];
-    const card = createCard(cardSrc);
-    console.log(card);
-    card_container.appendChild(card);
-}
 
+    for(let i=0; i< CARDS.length; i++ ){
+        const cardSrc = CARDS[i];
+        const card = createCard(cardSrc);
+        console.log(card);
+        card_container.appendChild(card);
+    }
 
 function createCard(src){
     const card = document.createElement('img');
+    card.addEventListener('click', onCardClick)
     card.src = src
     return card;
 }
 
-console.log(CARDS);
 
+function onCardClick(event){
+    const card = event.currentTarget;
+    
+    const activeCard = document.querySelector('.active');
+    if(activeCard){
+        activeCard.classList.remove('active');
+    }
+    card.classList.add('active');
+}
