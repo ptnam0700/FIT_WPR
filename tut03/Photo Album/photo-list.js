18e14fd0f0f1378983f4ad22185b2be5a9f68968
Nam Pham <ptnam0700@gmail.com>
2021-11-05 21:13:23 +0700

    Finish

--- tut03/Photo Album/photo-list.js
+++ tut03/Photo Album/photo-list.js
@@ -1,6 +1,7 @@
 const PHOTO_LIST = [
-    'images\IMG_4749.JPG',
-    'images\IMG_4772.JPG',
-    'images\IMG_4773.JPG',
-    'images\IMG_5605.JPG'
-]
+    'images/IMG_4749.JPG',
+    'images/IMG_4772.JPG',
+    'images/IMG-4773.JPG',
+    'images/IMG-5605.JPG' 
+];
+
--- tut03/Photo Album/photo.js
+++ tut03/Photo Album/photo.js
@@ -1,3 +1,6 @@
+
+
+// Album view
 function createImage(src){
     const image = document.createElement('img');
     image.src = src;
@@ -7,12 +10,26 @@ function createImage(src){
 const albumView = document.querySelector('#album-view');
 for(let i = 0; i < PHOTO_LIST.length; i++){
     const photoSrc = PHOTO_LIST[i];
+    console.log(photoSrc)
     const image = createImage(photoSrc);
     image.addEventListener('click', onThumbnailClick);
     albumView.appendChild(image);
 }
 
+// Model view
+const modalView = document.querySelector('#model-view')
+modalView.addEventListener('click', onModalClick);
+
+function onModalClick(){
+    document.body.classList.remove('no-scroll')
+    modalView.classList.add('hidden');
+    modalView.innerHTML = "";
+}
+
 function onThumbnailClick(event){
     const image = createImage(event.currentTarget.src);
-    model
+    document.body.classList.add('no-scroll')
+    modalView.style.top = window.pageYOffset + 'px';
+    modalView.appendChild(image);
+    modalView.classList.remove('hidden');
 }
